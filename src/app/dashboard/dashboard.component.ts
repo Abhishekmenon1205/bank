87fab4eed33948500d61424859e25b6eba5d8e77
ABHISHEK MENON <abhishekkrishnan1205@gmail.com>
2023-02-08 20:19:59 +0530

    initial

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../services/data.service'
 
 @Component({
@@ -10,24 +11,42 @@ export class DashboardComponent implements OnInit {
 
   user:any
 
-  acno:any
-  psw:any
-  amnt:any
+  // acno:any
+  // psw:any
+  // amnt:any
 
-  acno1:any
-  psw1:any
-  amnt1:any
-  constructor(private ds:DataService){
+  // acno1:any
+  // psw1:any
+  // amnt1:any
+  constructor(private ds:DataService,private fb:FormBuilder){
     this.user=this.ds.currentUser
   }
+
+ depositForm=this.fb.group({
+    acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    amnt:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+
+ })
+ withdrawForm=this.fb.group({
+  acno1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  amnt1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  psw1:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+
+})
+
   ngOnInit(): void{
     
     
   }
   deposit(){
-    var acno=this.acno
-    var psw=this.psw
-    var amnt=this.amnt
+    var acno=this.depositForm.value.acno
+    var psw=this.depositForm.value.psw
+    var amnt=this.depositForm.value.amnt
+
+    if (this.depositForm.valid) {
+      
+    
     const result=this.ds.deposit(acno,psw,amnt)
     if (result) {
       alert(`Your account has been credited with amount ${amnt} and the current balance is ${result}`)
@@ -36,14 +55,26 @@ export class DashboardComponent implements OnInit {
       alert("Incorrect account number or password")
     }
   }
-  withdraw(){
-    var acno=this.acno1
-    var psw=this.psw1
-    var amnt=this.amnt1
+  else{
+    alert("Invalid Form")
+  }
+}
 
+
+
+  withdraw(){
+    var acno=this.withdrawForm.value.acno1
+    var psw=this.withdrawForm.value.psw1
+    var amnt=this.withdrawForm.value.amnt1
 const result=this.ds.withdraw(acno,psw,amnt)
+if (this.depositForm.valid) {
+  
 if (result) {
   alert(`Your account has been debited with amount ${amnt} and the current balance is ${result}`)
 }
-  }
+}
+else{
+  alert("Invalid Form")
+}
+}
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,25 +10,30 @@ import { DataService } from '../services/data.service';
 })
 export class RegisterComponent implements OnInit {
 
-  uname=''
-acno=''
-psw=''
 
+
+  constructor(private ds:DataService, private router:Router,private fb:FormBuilder) { }
 // create reactive form of register form
 
+registerForm=this.fb.group({
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]],
+  psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
 
-  constructor(private ds:DataService, private router:Router) { }
+})
+
 
   ngOnInit(): void{
 
   }
   register() {
   
-   var uname=this.uname
-   var acno=this.acno
-   var psw=this.psw
+   var uname=this.registerForm.value.uname
+   var acno=this.registerForm.value.acno
+   var psw=this.registerForm.value.psw
   
-
+if (this.registerForm.valid) {
+  
   const result=this.ds.register(uname,acno,psw)
   
   if(result){
@@ -41,5 +47,9 @@ psw=''
   // console.log(uname,acno,psw);
   
   }
+  else{
+    alert('invalid form')
+  }
 
+}
 }
